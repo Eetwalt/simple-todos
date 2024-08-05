181e16b1fad16048c30d34d776e4bb03fe0f3dc0
Eetu Rantanen <eetu@eeturantanen.fi>
2024-08-06 00:43:18 +0300

    wip

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,4 +10,9 @@ class Task extends Model
     use HasFactory;
 
     protected $guarded = ['id', 'user_id', 'created_at', 'updated_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
