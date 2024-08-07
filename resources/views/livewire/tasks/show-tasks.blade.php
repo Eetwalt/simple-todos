966a00acf507c84eeb7e024d22fc17313873139a
Eetu Rantanen <eetu@eeturantanen.fi>
2024-08-07 12:42:09 +0300

    wip

--- resources/views/livewire/tasks/show-tasks.blade.php
+++ resources/views/livewire/tasks/show-tasks.blade.php
@@ -21,6 +21,13 @@ new class extends Component {
             'doneTasks' => $doneTasks,
         ];
     }
+
+    public function updateStatus($id, $status)
+    {
+        $task = Auth::user()->tasks()->find($id);
+        $task->status = $status;
+        $task->save();
+    }
 };
 
 ?>
@@ -30,7 +37,7 @@ new class extends Component {
     <div class="grid grid-cols-3 gap-4">
         <div class="space-y-2">
             <p class="text-sm font-semibold text-gray-800 uppercase">Todo</p>
-            <div class="flex flex-col gap-6 p-4 border border-gray-500 rounded-lg bg-slate-100">
+            <div class="flex flex-col gap-4 p-4 border border-gray-500 rounded-lg bg-slate-100">
                 @foreach ($todoTasks as $task)
                     <x-wui-card wire:key="{{ $task->id }}">
                         <x-slot name="title" class="flex flex-col w-full gap-2">
@@ -49,6 +56,10 @@ new class extends Component {
                         <x-slot name="slot" class="text-sm">
                             {{ $task->description }}
                         </x-slot>
+                        <x-slot name="footer" class="flex justify-end w-full gap-1">
+                            <x-wui-mini-button rounded icon="chevron-right" outline black xs
+                                wire:click="updateStatus({{ $task->id }}, 'In progress')" />
+                        </x-slot>
                     </x-wui-card>
                 @endforeach
             </div>
@@ -56,24 +67,24 @@ new class extends Component {
         <div class="space-y-2">
             <p class="text-sm font-semibold text-gray-800 uppercase">In progress</p>
 
-            <div class="flex flex-col gap-6 p-4 bg-blue-100 border border-blue-500 rounded-lg">
+            <div class="flex flex-col gap-4 p-4 overflow-hidden bg-blue-100 border border-blue-500">
                 @foreach ($inProgressTasks as $task)
                     <x-wui-card wire:key="{{ $task->id }}">
-                        <x-slot name="title" class="flex flex-col w-full gap-2">
-                            <div class="flex items-center justify-between w-full">
-                                @if ($task->due_date)
-                                    <p class="text-xs text-gray-500">
-                                        Due: {{ \Carbon\Carbon::parse($task->due_date)->format('d/m/Y') }}
-                                    </p>
-                                @endif
-                            </div>
-
+                        <x-slot name="title" class="flex items-center justify-between w-full gap-2">
                             <a href="#" class="text-sm font-bold text-gray-900 underline underline-offset-2">
                                 {{ $task->title }}
                             </a>
+                            <x-wui-mini-button rounded icon="check" flat positive
+                                wire:click="updateStatus({{ $task->id }}, 'Done')" />
                         </x-slot>
                         <x-slot name="slot" class="text-sm">
                             {{ $task->description }}
+                        </x-slot>
+                        <x-slot name="slot" class="flex justify-between w-full gap-1 py-[8px] bg-gray-50 rounded-lg">
+                            <x-wui-mini-button rounded icon="chevron-left" outline black xs
+                                wire:click="updateStatus({{ $task->id }}, 'Todo')" />
+                            <x-wui-mini-button rounded icon="clock" outline black xs
+                                wire:click="updateStatus({{ $task->id }}, 'Waiting')" />
                         </x-slot>
                     </x-wui-card>
                 @endforeach
@@ -82,7 +93,7 @@ new class extends Component {
         <div class="space-y-2">
             <p class="text-sm font-semibold text-gray-800 uppercase">Waiting</p>
 
-            <div class="flex flex-col gap-6 p-4 bg-yellow-100 border border-yellow-500 rounded-lg">
+            <div class="flex flex-col gap-4 p-4 bg-yellow-100 border border-yellow-500 rounded-lg">
                 @foreach ($waitingTasks as $task)
                     <x-wui-card wire:key="{{ $task->id }}">
                         <x-slot name="title" class="flex flex-col w-full gap-2">
@@ -108,7 +119,7 @@ new class extends Component {
     </div>
     <div class="mt-6 space-y-2">
         <p class="text-sm font-semibold text-gray-800 uppercase">Done</p>
-        <div class="grid grid-cols-3 gap-6 p-4 bg-green-100 border border-green-500 rounded-lg">
+        <div class="grid grid-cols-3 gap-4 p-4 bg-green-100 border border-green-500 rounded-lg">
             @foreach ($doneTasks as $task)
                 <x-wui-card wire:key="{{ $task->id }}">
                     <x-slot name="title" class="flex flex-col w-full gap-2">
--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -7,8 +7,34 @@
 
     <div class="py-12">
         <div class="flex flex-col gap-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <x-wui-button label="Open" wire:click="$openModal('simpleModal')" primary />
+
             <livewire:tasks.create-task />
             <livewire:tasks.show-tasks />
         </div>
     </div>
+
+    <x-wui-modal name="simpleModal" persistent>
+        <x-wui-card title="Consent Terms">
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                type
+                and scrambled it to make a type specimen book. It has survived not only five centuries, but also
+                the
+                leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
+                1960s
+                with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
+                desktop
+                publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+
+            <x-slot name="footer" class="flex justify-end gap-x-4">
+                <x-wui-button flat label="Cancel" x-on:click="close" />
+
+                <x-wui-button primary label="I Agree" wire:click="agree" />
+            </x-slot>
+        </x-wui-card>
+    </x-wui-modal>
 </x-app-layout>
