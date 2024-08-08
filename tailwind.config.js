094762fe25421053d6fddafb7af20efe447a46d3
Eetwalt <eetu@eeturantanen.fi>
2024-08-08 12:22:42 +0300

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,11 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    presets: [
-        "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
-    ],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
     ],
 
     theme: {
